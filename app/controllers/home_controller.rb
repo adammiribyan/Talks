439c40e3bd2405d543fe928bfa833e3a019a6991
Adam <adam.miribyan@gmail.com>
2010-12-08 16:54:12 +0300

    Main page almost done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   
   def index
-    @features = Post.featured.all :order => 'created_at desc', :limit => 2
-    @posts = Post.all :order => 'created_at desc'
+    @features = Post.featured.all :order => 'updated_at desc', :limit => 2
+    @latest_posts = Post.all :order => 'created_at desc'
+    @popular_posts = Post.all :order => 'created_at desc', :limit => 4
   end
   
 end
