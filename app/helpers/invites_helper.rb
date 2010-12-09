791c45eb60a63ff82f31e9d7496216a044a29b99
Adam <adam.miribyan@gmail.com>
2010-12-09 15:04:33 +0300

    Invite sending form basement

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -1,2 +1,17 @@
+# coding: utf-8
+
 module InvitesHelper
+  
+  def invite_letter_subject
+    if current_user
+      if current_user.firstname.empty?
+        name = ""
+      else
+        name = "(#{current_user.fullname})"
+      end
+      
+      return "Тема: <span>#{current_user.username.to_s} #{name.to_s} приглашает вас на Talks</span>"
+    end
+  end
+  
 end
