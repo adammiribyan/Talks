13d4121b1e6b89be39506f925e78f12c12b0e419
Adam <adam.miribyan@gmail.com>
2010-12-10 10:08:26 +0300

    Invitation sending form: Done

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -14,4 +14,12 @@ module InvitesHelper
     end
   end
   
+  def invites_left_count
+    if current_user and current_user.invites_limit > 0      
+      declinated_name = Russian.p(current_user.invites_limit, "приглашение", "приглашения", "приглашений")
+      
+      return "У вас <strong>#{current_user.invites_limit}</strong> #{declinated_name}"
+    end
+  end
+  
 end
