10386b92af151ada1667ceb4a5d1b29c37dba2b7
Adam <adam.miribyan@gmail.com>
2011-01-04 00:34:15 +0300

    SQLite to MySQL

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,19 +26,6 @@ namespace :deploy do
   end
 end
 
-task :after_update_code, :roles => [:web, :db, :app] do
-  gitignore_lines = (Pathname(__FILE__).dirname.parent + '.gitignore').readlines.map{ |s| s.strip }
-  shared_dirs = gitignore_lines.grep(/^\/?public\/.+/)
-  shared_dirs.each do |dir|
-    from = Pathname("#{shared_path}/#{dir}").cleanpath
-    to = Pathname("#{release_path}/#{dir}").cleanpath
-    run "mkdir -p #{from}; mkdir -p #{to.parent}; ln -nfs #{from} #{to}"
-  end
-
-  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-# run "ln -nfs #{shared_path}/config/initializers/facebook.rb #{release_path}/config/initializers/facebook.rb"
-end
-
 
 task :after_symlink, :roles => [:web, :app] do
   run "perl -i -pe \"s/# ENV\\['RAILS_ENV'\\] \\|\\|= 'production'/ENV['RAILS_ENV'] ||= 'production'/\" #{current_path}/config/environment.rb"
