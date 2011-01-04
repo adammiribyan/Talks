357e3e9a00b77d8ff20c130fa6096cbe720a61ec
Adam <adam.miribyan@gmail.com>
2011-01-04 04:40:19 +0300

    database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,8 @@ namespace :deploy do
   
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/assets #{current_path}/public/assets"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
   end
   
   desc "Sync the public/assets directory."
@@ -47,3 +47,5 @@ task :console, :roles => :app do
     channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
   end
 end
+
+after "deploy:update_code", "deploy:symlink_shared"
